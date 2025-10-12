c613bf814b27a2f536b3ad4730166f14b418d593
dinesh7271 <mdineskumar7271@gmail.com>
2025-10-12 19:28:20 +0530

    text aligned

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 var addbutton = document.getElementById("add-button")
 var overlay = document.querySelector(".popupoverlay")
 var popupbox = document.querySelector(".popup-box")
-addbutton.addEventListener("click", function(){
-        popupbox.style.display="block"
-        overlay.style.display="block"
-    })
+addbutton.addEventListener("click", function () {
+    popupbox.style.display = "block"
+    overlay.style.display = "block"
+})
 
-var addbtn = document.getElementById("add-btn") 
-var cancelbtn = document.getElementById("cancel-btn") 
+var addbtn = document.getElementById("add-btn")
+var cancelbtn = document.getElementById("cancel-btn")
 
-addbtn.addEventListener("click", function(event){
-     event.preventDefault();
-     var div = document.createElement("div")
-     div.setAttribute("class", "box-container")
-     div.innerHTML=`<h2>${animename.value}</h2>
+addbtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    var div = document.createElement("div")
+    div.setAttribute("class", "box-container")
+    div.innerHTML = `<h2>${animename.value}</h2>
      <h4>${animeauthor.value}</h4>
      <p>${description.value}</p>
      <button onclick="deleteanime(event)">Delete</button>`
-     container.append(div)
-     popupbox.style.display="none"
-    overlay.style.display="none"
+    container.append(div)
+    popupbox.style.display = "none"
+    overlay.style.display = "none"
 })
 
-cancelbtn.addEventListener("click", function(event){
+cancelbtn.addEventListener("click", function (event) {
     event.preventDefault();
-    popupbox.style.display="none"
-    overlay.style.display="none"
+    popupbox.style.display = "none"
+    overlay.style.display = "none"
 })
 
 var container = document.querySelector(".container")
@@ -33,6 +33,6 @@ var animename = document.getElementById("animename")
 var animeauthor = document.getElementById("animeauthor")
 var description = document.getElementById("description")
 
-function deleteanime(event){
+function deleteanime(event) {
     event.target.parentElement.remove()
 }
